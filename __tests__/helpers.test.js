e2f2bce8ecfe2f3cb5d0ee0f6d10e7c2fb8af78f
santanam23 <mirmir11.ms@gmail.com>
2022-03-03 16:38:56 -0600

    seed created

---
+++ __tests__/helpers.test.js
@@ -0,0 +1,1 @@
+const {format_date, format_plural, format_url} = require('../utils/helpers');
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3300;
+const PORT = process.env.PORT || 3306;
+console.log(process.env);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
